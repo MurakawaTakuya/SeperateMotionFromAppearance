ae75afa57d220e4b84e51b280920dd04d40d8b6c
MurakawaTakuya <murakawatakuya5@gmail.com>
2025-10-07 18:20:33 +0900

    config作成

--- infer.sh
+++ infer.sh
@@ -1,7 +1,10 @@
-python3 infer/infer.py \
-  --model "models/zeroscope_v2_576w" \
-  --prompt "A horse is running in the forest." \
-  --checkpoint_folder outputs/train/car/pretrained \
-  --checkpoint_index 800 \
-  --seed 8551187 \
-  --output_dir ./inference_results
+python infer/infer.py \
+    --model "models/zeroscope_v2_576w" \
+    --prompt "A person is skateboarding." \
+    --checkpoint_folder outputs/train/train_2025-10-05T13-13-18/ \
+    --checkpoint_index 1000 \
+    --output_dir ./inference_results/ \
+    --noise_prior 0. \
+    --seed 8551187 \
+    --num-frames 16 \
+    --lora_scale 1
